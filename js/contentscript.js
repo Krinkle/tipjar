da9d454dc1ba00690338475e9ae4e2a4577bcdb4
Timo Tijhof <krinklemail@gmail.com>
2020-10-18 01:15:24 +0100

    Support wildcard domains
    After checking the current domain, check the list again for the
    parent domain.
    
    Also:
    
    * Use location.hostname instead of `new URL(location.href).hostname`.
      Most properties of the modern-era URL object also exist directly
      on Location objects. This includes 'href', but also 'hostname'.
      Use this directly for simplicity.
    
    * Fix the (seemingly) broken entries for Gutenberg and Notepad,
      which included a schema and/or path slash, and thus cannot match
      a hostname.
    
    * Update sites.js to use parent domains by default where
      possible. For Mozilla and Internet Archive, this means the
      duplicates can now be removed. For Wikipedia, this means
      the tipjar now works also when viewing articles such as on
      en.wikipedia.org, and on zh.wikipedia.og.
    
    Fixes https://github.com/corbindavenport/tipjar/issues/15.

--- js/contentscript.js
+++ js/contentscript.js
@@ -8,7 +8,9 @@ const bitcoinTag = document.querySelector('meta[name="bitcoin"]')
 // Docs: https://web.archive.org/web/20181106180613/https://autotip.io/docs/microtip-meta-tag
 const cryptoTag = document.querySelector('meta[name="microtip"]')
 // Page hostname
-const hostname = new URL(window.location.href).hostname.toString()
+const hostname = window.location.hostname
+// e.g. "en.wikipedia.org" to "wikipedia.org"
+const parentHostname = hostname.split('.').slice(1).join('.')
 
 // Create object for all donation options
 var supportLinks = {}
@@ -19,6 +21,8 @@ if (donateTag && donateTag.getAttribute('content')) {
 }
 if (sitesObj.hasOwnProperty(hostname)) {
     supportLinks.tipjarUrl = sitesObj[hostname]
+} else if (sitesObj.hasOwnProperty(parentHostname)) {
+    supportLinks.tipjarUrl = sitesObj[parentHostname]
 }
 if (scrollJS) {
     supportLinks.scroll = true
